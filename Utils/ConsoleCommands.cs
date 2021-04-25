2b52a3891dca3f78f5ab1909929466f37cacc8ac
Rich Dunne <richdunne@yahoo.com>
2021-04-25 07:29:38 -0600

    Added new commands

--- Utils/ConsoleCommands.cs
+++ Utils/ConsoleCommands.cs
@@ -7,7 +7,8 @@ using System.Reflection;
 namespace RichAmbiance.Utils
 {
     using RichAmbiance.Features;
-    
+    using System.Linq;
+
     [Obfuscation(Exclude = false, Feature = "-rename", ApplyToMembers = false)]
     internal static class ConsoleCommands
     {
@@ -18,15 +19,44 @@ namespace RichAmbiance.Utils
         }
 
         [ConsoleCommand("EndAmbientEvent")]
-        internal static void Command_EndAmbientEvent([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandAutoCompleterBoolean), Name = "EndAmbientEvent")] bool end = true)
+        internal static void Command_EndAmbientEvent([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandParameterAutoCompleterEnum), Name = "EndAmbientEvent")] EventType eventType)
         {
-            if (AmbientEvents.ActiveEvent == null)
+            if (AmbientEvents.ActiveEvents.Count == 0)
             {
-                Game.LogTrivial($"ActiveEvent is null.");
+                Game.LogTrivial($"There are no active events.");
                 return;
             }
 
-            AmbientEvents.ActiveEvent.TransitionToState(State.Ending);
+            if (!AmbientEvents.ActiveEvents.Any(x => x.EventType == eventType))
+            {
+                Game.LogTrivial($"There are no active {eventType} events.");
+                return;
+            }
+
+            AmbientEvents.ActiveEvents.Where(x => x.EventType == eventType).ToList().ForEach(x => x.TransitionToState(State.Ending));
+        }
+
+        [ConsoleCommand("EndAllAmbientEvents")]
+        internal static void Command_EndAllAmbientEvents()
+        {
+            Game.LogTrivial($"[Rich Ambiance]: Ending {AmbientEvents.ActiveEvents.Count()} active events.");
+            AmbientEvents.ActiveEvents.ForEach(x => x.TransitionToState(State.Ending));
+            Game.LogTrivial($"[Rich Ambiance]: All events terminated.");
+        }
+
+        [ConsoleCommand("GetActiveAmbientEvents")]
+        internal static void Command_GetActiveAmbientEvents()
+        {
+            if(AmbientEvents.ActiveEvents.Count == 0)
+            {
+                Game.LogTrivial($"[Rich Ambiance]: There are no events active.");
+                return;
+            }
+
+            AmbientEvents.ActiveEvents.ForEach(x =>
+            {
+                Game.LogTrivial($"[Rich Ambiance]: Current active event: {x.EventType}");
+            });
         }
     }
 }

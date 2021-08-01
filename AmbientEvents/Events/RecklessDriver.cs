32dca6d16d6c06b5ffc258d2df40b5eb21a2de3d
Rich Dunne <richdunne@yahoo.com>
2021-08-01 13:38:53 -0600

    Fixed event ending prematurely, added chance for driver to be on cell phone

--- AmbientEvents/Events/RecklessDriver.cs
+++ AmbientEvents/Events/RecklessDriver.cs
@@ -21,8 +21,6 @@ namespace RichAmbiance.AmbientEvents.Events
             {
                 return;
             }
-
-            TransitionToState(State.Ending, 3000);
         }
 
         private void Prepare()
@@ -44,9 +42,49 @@ namespace RichAmbiance.AmbientEvents.Events
 
         private void Process()
         {
+            TransitionToState(State.Running);
+            GameFiber.StartNew(() => CheckEndConditions(), "Rich Ambiance RecklessDriver End Conditions Fiber");
+
             _suspectVehicle.Driver.Tasks.Clear();
+            if(MathHelper.GetChance(3))
+            {
+                Game.LogTrivial($"[Rich Ambiance (Minor Event)]: Suspect is on their phone.");
+                Rage.Native.NativeFunction.Natives.TASK_USE_MOBILE_PHONE_TIMED(_suspectVehicle.Driver, 100000);
+            }
             _suspectVehicle.Driver.Tasks.DriveToPosition(new Vector3(new Random().Next(1000), new Random().Next(1000), 0), new Random().Next(5, 31), VehicleDrivingFlags.Emergency);
             Game.LogTrivial($"[Rich Ambiance (Minor Event)]: Suspect is driving recklessly.");
+        }
+
+        private void CheckEndConditions()
+        {
+            while (State != State.Ending)
+            {
+                GameFiber.Yield();
+                if (!_suspectVehicle || !_suspectVehicle.Driver || !_suspectVehicle.Driver.IsAlive)
+                {
+                    Game.LogTrivial($"[Rich Ambiance (Minor Event)]: Suspect or suspect vehicle no longer valid.  Ending event.");
+                    TransitionToState(State.Ending);
+                    return;
+                }
+                if (!Game.LocalPlayer.Character.IsAlive)
+                {
+                    Game.LogTrivial($"[Rich Ambiance (Minor Event)]: Player died.  Ending event.");
+                    TransitionToState(State.Ending);
+                    return;
+                }
+                if (LSPD_First_Response.Mod.API.Functions.IsPlayerPerformingPullover() || LSPD_First_Response.Mod.API.Functions.IsPedInPursuit(_suspectVehicle.Driver))
+                {
+                    Game.LogTrivial($"[Rich Ambiance (Minor Event)]: Player is performing pullover or suspect is in a pursuit.  Ending event.");
+                    TransitionToState(State.Ending);
+                    return;
+                }
+                if (_suspectVehicle.Driver.DistanceTo2D(Game.LocalPlayer.Character) > 150f)
+                {
+                    Game.LogTrivial($"[Rich Ambiance (Minor Event)]: Player is too far from suspect.  Ending event.");
+                    TransitionToState(State.Ending);
+                    return;
+                }
+            }
         }
     }
 }
